9f2cb71f39dc9dfc9786443163a8d101ebd384e3
Aneesh Bhat <aneeshbhatb@gmail.com>
2022-07-25 12:03:21 +0530

    Initial Commit

---
+++ lib/src/index.d.ts
@@ -0,0 +1,2 @@
+declare const getMediaDevices: () => void;
+export { getMediaDevices };
---
+++ src/index.ts
@@ -0,0 +1,21 @@
+const getMediaDevices = () => {
+  if (!navigator.mediaDevices || !navigator.mediaDevices.enumerateDevices) {
+    console.log('enumerateDevices() not supported.');
+  } else {
+    // List cameras and microphones.
+    navigator.mediaDevices
+      .enumerateDevices()
+      .then(devices => {
+        devices.forEach(device => {
+          console.log(
+            `${device.kind}: ${device.label} id = ${device.deviceId}`
+          );
+        });
+      })
+      .catch(err => {
+        console.log(`${err.name}: ${err.message}`);
+      });
+  }
+};
+
+export {getMediaDevices};
